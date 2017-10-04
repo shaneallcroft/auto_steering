dfef5c0451298a2f6f3ed3aa6f1b4ffca06946bf
Hunter Allen <hunter@openrobotics.org>
2017-10-03 23:32:45 -0400

    Fix build (#2)
    * Remove unneeded build directory from repository.
    
    * Enable C++14.
    
    * Fix CMake file for building.

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shane/catkin_fn/src/auto_steering
-# Build directory: /home/shane/catkin_fn/src/auto_steering/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/shane/catkin_fn/src/auto_steering/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
