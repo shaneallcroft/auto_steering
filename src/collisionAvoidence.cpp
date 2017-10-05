66af040c9fe75bdccca6f1fae903c08ab5260979
Shane Byrd Allcroft <shaneallcroft@gmail.com>
2017-10-04 21:23:42 -0400

    Reindent

--- src/collisionAvoidence.cpp
+++ src/collisionAvoidence.cpp
@@ -4,44 +4,46 @@
 
 class SubAndPub
 {
-public:
-	SubAndPub()
-	{
-		pub = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
+  public:
+  SubAndPub() {
+      pub = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
 
-		sub = n.subscribe("/scan",10, &SubAndPub::cmdVelCallback, this);
-	}
-	void cmdVelCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg)
-	{
-		geometry_msgs::Twist velMsg;
-		if(scan_msg->ranges[540] < 2)
-		{
-			velMsg.linear.x = 0.0;
-			velMsg.angular.z = 1.8;
-		}
-		else
-		{
-			velMsg.linear.x = 1.5;
-			velMsg.angular.z = 1.8;
-		}
-		pub.publish(velMsg);
+      sub = n.subscribe("/scan",10, &SubAndPub::cmdVelCallback, this);
+  }
+  void cmdVelCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg);
 
-	}
-private:
-	ros::NodeHandle n;
-	ros::Publisher pub;
-	ros::Subscriber sub;
+  private:
+  ros::NodeHandle n;
+  ros::Publisher pub;
+  ros::Subscriber sub;
 };
-	int main(int argc, char **argv)
-	{
 
-		ros::init(argc, argv, "collisionAvoidence");
+void SubAndPub::cmdVelCallback(const sensor_msgs::LaserScan::ConstPtr& scan_msg)
+{
+  geometry_msgs::Twist velMsg;
 
-		SubAndPub substerAndPubster;
-		ros::spin();
+  if(scan_msg->ranges[540] < 1.0 &&
+     scan_msg->ranges[539] < 1.0 &&
+     scan_msg->ranges[538] < 1.0 &&
+     scan_msg->ranges[541] < 1.0) {
+    ROS_INFO("TURN!!!");
+    velMsg.linear.x = 0.0;
+    velMsg.angular.z = .8;
+  } else {
+    ROS_INFO("FORWARD");
+    velMsg.linear.x = .5;
+    velMsg.angular.z = 0.0;
+  }
 
-		return 0;
+  pub.publish(velMsg);
+}
 
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "collisionAvoidence");
 
+  SubAndPub substerAndPubster;
+  ros::spin();
 
-	}
+  return 0;
+}
